8d93bfe70ce7d50ab0e7ff1a373fd265fc940921
Arsalan Danish <arsalan.jamia@gmail.com>
2023-12-19 12:38:42 +0530

    Astrology-App

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,189 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
+import './style.css'
+import { useState } from 'react'
+import { numberChart,destinyNumberDefinations,soulNumberDefinations,dreamNumberDefinatons } from './data';
+
+
+const Home = () => {
+
+  const [name, setName] = useState('');
+  const [error, setError] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    const isValid = /^[A-Za-z\s]+$/.test(inputValue);
+
+    setName(inputValue);
+
+    if (isValid) {
+      setError('');
+    } else {
+      setError('Please enter a valid name (letters and spaces only).');
+      setSubmitted(false)
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitted Name:', name);
+    setSubmitted(true);
+  };
+
+  const handleReset = () => {
+    setName('');
+    setSubmitted(false);
+
+  }
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const containerStyle = {
+    backgroundColor: darkMode ? '#222' : '#fff',
+    color: darkMode ? '#fff' : '#000',
+  };
+
+  let nameStr = name.toString().toUpperCase();
+  let str = "";
+
+  for (let i = 0; i < nameStr.length; i++) {
+    if (("A" <= nameStr[i]) && ("Z" >= nameStr[i])) {
+      str += nameStr[i];
+    }
+  }
+
+  let arr = [];
+  let sum = 0;
+  let numbers = [];
+  let vowelsNum = [];
+  let consonantNum = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toUpperCase();
+    arr.push(char);
+    if (numberChart[char]) {
+      numbers.push(numberChart[char]);
+      sum += numberChart[char]
+    }
+    if (["A", "E", "I", "O", "U"].includes(char)) {
+      vowelsNum.push(numberChart[char]);
+    }
+    if (!["A", "E", "I", "O", "U"].includes(char)) {
+      consonantNum.push(numberChart[char]);
+    }
+  }
+
+  function calculateSingleDigitSum(array) {
+    let totalSum = array.reduce((acc, num) => acc + num, 0);
+
+    while (totalSum >= 10 && ![11, 22, 33].includes(totalSum)) {
+      let digitSum = 0;
+
+      while (totalSum > 0) {
+        digitSum += totalSum % 10;
+        totalSum = Math.floor(totalSum / 10);
+      }
+
+      totalSum = digitSum;
+    }
+
+    return totalSum;
+  }
+
+  const total = calculateSingleDigitSum(numbers);
+  const totalVowel = calculateSingleDigitSum(vowelsNum);
+  const totalConsont = calculateSingleDigitSum(consonantNum);
+
+  const getdestinyNumberDefinitions = (number) => {
+    return destinyNumberDefinations[number] || '';
+  };
+  const getsoulNumberDefinitions = (number) => {
+    return soulNumberDefinations[number] || '';
+  };
+  const getdreamNumberDefinitions = (number) => {
+    return dreamNumberDefinatons[number] || '';
+  };
+
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={`container ${darkMode ? 'dark' : 'light'}`} style={containerStyle}>
+      <div className='mode' onClick={toggleDarkMode}>
+        {darkMode ? (
+          <div className='darkMode-images' role="img" aria-label="Sun">
+            ☀️
+          </div>
+        ) : (
+          <div className='darkMode-images' role="img" aria-label="Moon">
+            🌙
+          </div>
+        )}
+      </div>
+      <div className="astrology_image">
+        <img src="https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/48725/optimized_product_thumb_stage.jpg" alt="" />
+      </div>
+      <div className="main">
+        <h2>Online Name Numerology Calculator</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter Your Name:
+            <input type="text" value={name} onChange={handleChange} />
+          </label>
+          <p style={{ color: 'red' }}>{error}</p>
+          <div className="buttons-block">
+            <button type="submit" disabled={!name}>
+              Calculate
+            </button>
+            <button type="reset" onClick={handleReset}>
+              Reset
+            </button>
+
+          </div>
+
+        </form>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        
+        {submitted && (
+          <>
+            <div className="userDetails">
+              <h1>Chaldean Name Numerology Reading of <span style={{ color: "gold" }}>{name} !</span></h1>
+              <h3>Compound Name Number/Namank (Numerology Total of Your Name): <span className='totalSum'>{sum}</span></h3>
+              <div className="userName">
+                <h4>{str.toUpperCase()}</h4>
+                <h5>{JSON.stringify(numbers)}</h5>
+              </div>
+              <div className="destiny">
+                <p><b>Destiny Number :</b> The Expression number, which describes who you are, and what you are, or what you become.</p>
+                <p> Name Destiny/Expression Number or Namanak :- <span className='resultSum'>{total}</span></p>
+                <h4>{getdestinyNumberDefinitions(total)}</h4>
+              </div>
+              <div className="destiny">
+                <p><b>Soul Urge Number :</b>The Heart Desire number, which describes your inner potentials and inner resources.</p>
+                <p> Soul Urge/Heart Desire Number :- <span className='resultSum'>{totalVowel}</span></p>
+                <h4>{getsoulNumberDefinitions(totalVowel)}</h4>
+              </div>
+
+              <div className="destiny">
+                <p><b>Dream Number :</b>: The Personality number which describes outer personality, indeed your first impression on others.</p>
+                <p> Name Dream/Personality Number :- <span className='resultSum'>{totalConsont}</span></p>
+                <h4>{getdreamNumberDefinitions(totalConsont)}</h4>
+
+              </div>
+
+
+            </div>
+
+          </>
+        )}
       </div>
+    </div>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
